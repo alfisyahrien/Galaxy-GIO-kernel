afd5aee3285c47819b164cd4771eb51e9a20b5da
Erick <delano.erick@gmail.com>
2011-12-28 14:59:07 +0700

    first commit

---
+++ source/include/generated/compile.h
@@ -0,0 +1,9 @@
+/* This file is auto generated, version 5 */
+/*  PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#5 PREEMPT Wed Dec 28 09:35:16 WIT 2011"
+#define LINUX_COMPILE_TIME "09:35:16"
+#define LINUX_COMPILE_BY "delano"
+#define LINUX_COMPILE_HOST "delanoister_ANDRO-ID_dev"
+#define LINUX_COMPILE_DOMAIN "(none)"
+#define LINUX_COMPILER "gcc version 4.4.1 (Sourcery G++ 4.4-180) "
---
+++ source/make_recovery_kernel_GT-S5660.sh
@@ -0,0 +1,4 @@
+export CCOMPILER=~/android/sources/gio-kernel/CodeSourcery/Sourcery_G++/bin/arm-none-eabi-
+make ARCH=arm CROSS_COMPILE=$CCOMPILER gio_rev03_recovery_defconfig
+make ARCH=arm CROSS_COMPILE=~/android/sources/gio-kernel/CodeSourcery/Sourcery_G++/bin/arm-none-eabi-
+
